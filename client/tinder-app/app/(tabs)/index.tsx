1483c6c5222346e5eb5c52f6e99e62792dcca019
gracenev <144965499+gracenev@users.noreply.github.com>
2025-01-11 20:20:36 -0500

    Updated profile page

--- client/tinder-app/app/(tabs)/index.tsx
+++ client/tinder-app/app/(tabs)/index.tsx
@@ -1,58 +1,90 @@
-import { Image, StyleSheet, Platform } from "react-native";
+import { Image, StyleSheet, Platform, TextInput } from "react-native";
 
 import { HelloWave } from "@/components/HelloWave";
 import ParallaxScrollView from "@/components/ParallaxScrollView";
 import { ThemedText } from "@/components/ThemedText";
 import { ThemedView } from "@/components/ThemedView";
+import { useState } from "react";
 
 export default function HomeScreen() {
+  const [fName, setfName] = useState("");
+  const [lName, setlName] = useState("");
+  const [email, setEmail] = useState("");
+  const [age, setAge] = useState("");
+  const [school, setSchool] = useState("");
+  const [year, setYear] = useState("");
+  const [program, setProgram] = useState("");
+
   return (
     <ParallaxScrollView
       headerBackgroundColor={{ light: "#A1CEDC", dark: "#1D3D47" }}
-      headerImage={
-        <Image
-          source={require("@/assets/images/partial-react-logo.png")}
-          style={styles.reactLogo}
-        />
-      }
+      headerImage={<Image style={styles.reactLogo} />}
     >
       <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">HIIIIIII!</ThemedText>
-        <HelloWave />
+        <ThemedText type="title">My Profile</ThemedText>
       </ThemedView>
       <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 1: Try it</ThemedText>
-        <ThemedText>
-          Edit{" "}
-          <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText>{" "}
-          to see changes. Press{" "}
-          <ThemedText type="defaultSemiBold">
-            {Platform.select({
-              ios: "cmd + d",
-              android: "cmd + m",
-              web: "F12",
-            })}
-          </ThemedText>{" "}
-          to open developer tools.
-        </ThemedText>
+        <ThemedText type="subtitle">Enter Email</ThemedText>
+        <TextInput
+          placeholder="email"
+          keyboardType="default"
+          value={email}
+          onChangeText={(text) => setEmail(text)}
+        />
       </ThemedView>
       <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 2: Explore</ThemedText>
-        <ThemedText>
-          Tap the Explore tab to learn more about what's included in this
-          starter app.
-        </ThemedText>
+        <ThemedText type="subtitle">Enter First Name</ThemedText>
+        <TextInput
+          placeholder="first name"
+          keyboardType="default"
+          value={fName}
+          onChangeText={(text) => setfName(text)}
+        />
       </ThemedView>
       <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 3: Get a fresh start</ThemedText>
-        <ThemedText>
-          When you're ready, run{" "}
-          <ThemedText type="defaultSemiBold">npm run reset-project</ThemedText>{" "}
-          to get a fresh <ThemedText type="defaultSemiBold">app</ThemedText>{" "}
-          directory. This will move the current{" "}
-          <ThemedText type="defaultSemiBold">app</ThemedText> to{" "}
-          <ThemedText type="defaultSemiBold">app-example</ThemedText>.
-        </ThemedText>
+        <ThemedText type="subtitle">Enter Last Name</ThemedText>
+        <TextInput
+          placeholder="last name"
+          keyboardType="default"
+          value={lName}
+          onChangeText={(text) => setlName(text)}
+        />
+      </ThemedView>
+      <ThemedView style={styles.stepContainer}>
+        <ThemedText type="subtitle">Enter Age</ThemedText>
+        <TextInput
+          placeholder="age"
+          keyboardType="numeric"
+          value={age}
+          onChangeText={(text) => setAge(text)}
+        />
+      </ThemedView>
+      <ThemedView style={styles.stepContainer}>
+        <ThemedText type="subtitle">Enter School</ThemedText>
+        <TextInput
+          placeholder="school"
+          keyboardType="default"
+          value={school}
+          onChangeText={(text) => setSchool(text)}
+        />
+      </ThemedView>
+      <ThemedView style={styles.stepContainer}>
+        <ThemedText type="subtitle">Enter Program</ThemedText>
+        <TextInput
+          placeholder="program"
+          keyboardType="default"
+          value={program}
+          onChangeText={(text) => setProgram(text)}
+        />
+      </ThemedView>
+      <ThemedView style={styles.stepContainer}>
+        <ThemedText type="subtitle">Enter Year of Study</ThemedText>
+        <TextInput
+          placeholder="year of study"
+          keyboardType="numeric"
+          value={year}
+          onChangeText={(text) => setYear(text)}
+        />
       </ThemedView>
     </ParallaxScrollView>
   );
